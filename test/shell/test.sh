11ed52fae2086d19639adc7bbfb39eed21c367f9
Karim Hamza <karim.hamza@axa.fr>
2018-12-05 19:37:42 +0100

    test shell script

--- test/shell/test.sh
+++ test/shell/test.sh
@@ -1,9 +1,18 @@
 #!/bin/ksh
+echo
+echo
+echo ###########
+env
+echo ###########
+echo
+echo
 
 conf_file=/etc/apache2/apache2.conf
-if [ "${KITCHEN_INSTANCE}" == "default-centos-7" ]
+if [ "${INSTANCE}" == "default-centos-7" ]
 then
   conf_file=/etc/httpd/conf/httpd.conf
 fi
 
 cat ${conf_file}
+echo
+cat /etc/apache2/ports.conf
