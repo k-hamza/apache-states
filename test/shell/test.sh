c863b2ccef94a5100395cf143905515142f59a8e
karim Hamza <karim.hamza@axa.fr>
2018-12-06 18:06:25 +0100

    add debian

--- test/shell/test.sh
+++ test/shell/test.sh
@@ -7,4 +7,10 @@ echo ###########
 echo
 echo
 
-sudo salt-call --local grains.items
+# sudo salt-call --local grains.items
+if [ -e /etc/httpd/conf/httpd.conf ]
+then
+  cat /etc/httpd/conf/httpd.conf
+elif [ -e /etc/apache2/apache2.conf ]
+  cat /etc/apache2/apache2.conf
+fi
